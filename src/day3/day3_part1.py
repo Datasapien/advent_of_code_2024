f4f0c2ff3f9c9a6a2ceaaded7aa82e9680802842
Datasapien <bradleyclayton89@gmail.com>
2024-12-04 22:56:30 +0000

    Work in progress for day3_part1 - almost there!

--- src/day3/day3_part1.py
+++ src/day3/day3_part1.py
@@ -1,7 +1,78 @@
+from pprint import pprint
+import csv
+import re
+
 """
 Search day3_input for sequence mul(X,Y) where X and Y are integers
+mul(X,Y) multiples the integers to get a result (X*Y)
 
+Corrupted, similar sequences must be ignored:
+    mul(4*, mul(6,9!, ?(12,34), or mul ( 2 , 4 ) etc
+
+Given following string:
+
+    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+
+There are only 4 valid sequences:
+    mul(2,4)
+    mul(5,5)
+    mul(11,8)
+    mul(8,5)
+
+Adding their results together gives the answer:
+    161 (2*4 + 5*5 + 11*8 + 8*5)
+
+Calculate the total from day3_input.csv
 """
 
-def func():
-    pass
+def create_memory():
+
+    # Read day3_input.csv and output it
+
+    csv_file = 'data/test_input.csv'
+
+    with open(csv_file, 'r') as file:
+        string = csv.reader(file, delimiter = ' ')
+
+        corrupted_memory = []
+
+        for row in string:
+            
+            for item in row:
+                corrupted_memory.append(item)
+
+        print(corrupted_memory)        
+        return corrupted_memory
+    
+def multiplier_adder():
+    
+    total = 0
+
+    corrupted_memory = create_memory()
+
+    print(1, corrupted_memory)
+
+    pattern = r'mul\(\d+,\d+\)'
+
+    for memory_string in corrupted_memory:
+
+        print(2, memory_string)
+
+        multipliers = re.findall(pattern, memory_string)
+        print(3, multipliers)
+
+        for multiplier in multipliers:
+            print(4, multiplier)
+
+            pattern = r'mul\((\d+),(\d+)\)'
+
+            digits = re.match(pattern, multiplier).groups()
+
+            # print(digits)
+
+            total += int(digits[0]) * int(digits[1])
+
+    print(total)
+    return total
+
+create_memory()
--- test/test_day3_part1.py
+++ test/test_day3_part1.py
@@ -1,12 +1,34 @@
 import pytest
-from src.day3.day3_part1 import func
+from src.day3.day3_part1 import multiplier_adder
 from unittest.mock import patch
 
-@patch("")
-def test(mock):
+@patch("src.day3.day3_part1.create_memory")
+def xtest_returns_int(mock_create_memory):
 
-    mock.return_value = [[7, 6, 4, 2, 1]]
+    mock_create_memory.return_value = ["mul(4,5)"]
 
-    result = func()
+    result = multiplier_adder()
 
     assert type(result) == int
+
+@patch("src.day3.day3_part1.create_memory")
+def xtest_returns_correct_value_for_single_mul(mock_create_memory):
+
+    mock_create_memory.return_value = ["mul(4,5)"]
+
+    result = multiplier_adder()
+
+    expected = 20
+
+    assert result == expected
+
+@patch("src.day3.day3_part1.create_memory")
+def test_returns_correct_value_for_multiple_mul(mock_create_memory):
+
+    mock_create_memory.return_value = ["mul(4,5)","mul(3,6)"]
+
+    result = multiplier_adder()
+
+    expected = 38
+
+    assert result == expected
