4ab606365841275dc06a81a3855a2c599bad9352
Datasapien <bradleyclayton89@gmail.com>
2024-12-05 11:03:54 +0000

    Update docstring with instructions for day3_part2

---
+++ src/day3/day3_part2.py
@@ -0,0 +1,70 @@
+import csv
+import re
+
+"""
+Search day3_input for sequence mul(X,Y) where X and Y are integers
+mul(X,Y) multiples the integers to get a result (X*Y)
+
+Corrupted, similar sequences must be ignored:
+    mul(4*, mul(6,9!, ?(12,34), or mul ( 2 , 4 ) etc
+
+There are two new instructions you'll need to handle:
+
+The do() instruction enables future mul instructions.
+The don't() instruction disables future mul instructions.
+Only the most recent do() or don't() instruction applies. At the beginning of the program, mul instructions are enabled.
+
+Given following string:
+
+    xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+
+There are only TWO valid sequences:
+    mul(2,4)
+    mul(8,5)
+
+Adding their results together gives the answer:
+    161 (2*4 + 5*5 + 11*8 + 8*5)
+
+Calculate the total from day3_input.csv
+"""
+
+def create_memory():
+
+    csv_file = 'data/day3_input.csv'
+
+    with open(csv_file, 'r') as file:
+        string = csv.reader(file, delimiter = ' ')
+
+        corrupted_memory = []
+
+        for row in string:
+            
+            for item in row:
+                corrupted_memory.append(item)
+
+        return corrupted_memory
+    
+def multiplier_adder():
+    
+    total = 0
+
+    corrupted_memory = create_memory()
+
+    extract_mul_pattern = r'mul\(\d+,\d+\)'
+
+    for memory_string in corrupted_memory:
+
+        multipliers = re.findall(extract_mul_pattern, memory_string)
+
+        for multiplier in multipliers:
+
+            extract_digit_pattern = r'mul\((\d+),(\d+)\)'
+
+            digits = re.match(extract_digit_pattern, multiplier)
+
+            total += int(digits.group(1)) * int(digits.group(2))
+
+    print(total)
+    return total
+
+multiplier_adder()
---
+++ test/test_day3_part2.py
@@ -0,0 +1,56 @@
+import pytest
+from src.day3.day3_part1 import multiplier_adder
+from unittest.mock import patch
+
+@patch("src.day3.day3_part1.create_memory")
+def test_returns_int(mock_create_memory):
+
+    mock_create_memory.return_value = ["mul(4,5)"]
+
+    result = multiplier_adder()
+
+    assert type(result) == int
+
+@patch("src.day3.day3_part1.create_memory")
+def test_returns_correct_value_for_single_mul(mock_create_memory):
+
+    mock_create_memory.return_value = ["mul(4,5)"]
+
+    result = multiplier_adder()
+
+    expected = 20
+
+    assert result == expected
+
+@patch("src.day3.day3_part1.create_memory")
+def test_returns_correct_value_for_multiple_mul(mock_create_memory):
+
+    mock_create_memory.return_value = ["mul(4,5)","mul(3,6)"]
+
+    result = multiplier_adder()
+
+    expected = 38
+
+    assert result == expected
+
+@patch("src.day3.day3_part1.create_memory")
+def test_returns_correct_value_for_single_corrupt_str(mock_create_memory):
+
+    mock_create_memory.return_value = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"]
+
+    result = multiplier_adder()
+
+    expected = 161
+
+    assert result == expected
+
+@patch("src.day3.day3_part1.create_memory")
+def test_returns_correct_value_for_two_corrupt_strs(mock_create_memory):
+
+    mock_create_memory.return_value = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"]
+
+    result = multiplier_adder()
+
+    expected = 322
+
+    assert result == expected
